17abf28fd695c068d4e6a469e7b2f65474cd86c0
felec <felecrear@gmail.com>
2021-11-30 18:59:01 -0800

    add nav content

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,5 +1,13 @@
 import { useState } from 'react';
-import { MdKeyboardArrowDown, MdAccountCircle } from 'react-icons/md';
+import {
+  MdKeyboardArrowDown,
+  MdAccountCircle,
+  MdAdminPanelSettings,
+  MdAdsClick,
+  MdAssessment,
+  MdInfoOutline,
+  MdPeopleAlt,
+} from 'react-icons/md';
 
 interface NavItem {
   width: number;
@@ -49,15 +57,96 @@ export const HeaderNav = () => {
 const HeaderBox = ({ info }: { info: NavItem }) => {
   return (
     <div className='relative'>
-      <div className='absolute top-2 left-10 h-72 w-96 bg-white shadow-md rounded-md'></div>
+      <div className='absolute top-2 left-10 h-60 w-96 bg-white shadow-md rounded-md'></div>
 
-      <div className='absolute top-4 left-12'></div>
+      {/* <div className='products absolute top-2 left-10 px-4 h-60 w-96 flex flex-col justify-evenly'>
+        <div className='flex cursor-pointer'>
+          <div className='h-12 w-12 flex items-center justify-center bg-blue-300 rounded-full'>
+            <MdAdminPanelSettings size={30} color='#fff' />
+          </div>
+          <div className='flex flex-col ml-4'>
+            <h3 className='text-lg font-bold text-blue-500'>Recusandae</h3>
+            <p className='text-sm font-light text-gray-400'>
+              Laboriosam obcaecati ipsam explicabo.
+            </p>
+          </div>
+        </div>
 
-      {/* <div className='absolute top-4 left-12'>bjioew4yt</div>
+        <div className='flex cursor-pointer'>
+          <div className='h-12 w-12 flex items-center justify-center bg-green-300 rounded-full'>
+            <MdAdsClick size={30} color='#fff' />
+          </div>
+          <div className='flex flex-col ml-4'>
+            <h3 className='text-lg font-bold text-green-500'>Laudantium</h3>
+            <p className='text-sm font-light text-gray-400'>
+              Animi laudantium quae ea sit.
+            </p>
+          </div>
+        </div>
 
-      <div className='absolute top-4 left-12'>cjpoiqwe</div>
+        <div className='flex cursor-pointer'>
+          <div className='h-12 w-12 flex items-center justify-center bg-indigo-300 rounded-full'>
+            <MdAssessment size={30} color='#fff' />
+          </div>
+          <div className='flex flex-col ml-4'>
+            <h3 className='text-lg font-bold text-indigo-500'>Architecto</h3>
+            <p className='text-sm font-light text-gray-400'>
+              Praesentium adipisci rerum nihil.
+            </p>
+          </div>
+        </div>
+      </div> */}
 
-      <div className='absolute top-4 left-12'>dnkjrewoiq</div> */}
+      {/* <div className='features absolute top-2 left-10 px-12 py-4 h-60 w-96 flex flex-col justify-evenly'>
+        <div className='flex flex-col mb-4'>
+          <h3 className='text-2xl font-bold text-purple-600'>Architecto</h3>
+          <p className='text-sm font-light text-gray-400'>
+            Praesentium adipisci rerum nihil adipisicing.
+          </p>
+        </div>
+
+        <div className='flex justify-between h-1/2'>
+          <div className='flex flex-col text-sm justify-between'>
+            <h3 className='text-gray-400 uppercase'>Get Started</h3>
+            <h4 className='text-purple-500 hover:opacity-50 cursor-pointer'>
+              aspernatur
+            </h4>
+            <h4 className='text-purple-500 hover:opacity-50 cursor-pointer'>
+              necessitatibus
+            </h4>
+            <h4 className='text-purple-500 hover:opacity-50 cursor-pointer'>
+              deleniti
+            </h4>
+          </div>
+
+          <div className='flex flex-col text-sm justify-between'>
+            <h3 className='text-gray-400 uppercase'>Help</h3>
+            <h4 className='text-purple-500 hover:opacity-50 cursor-pointer'>
+              cupiditate
+            </h4>
+            <h4 className='text-purple-500 hover:opacity-50 cursor-pointer'>
+              blanditiis
+            </h4>
+            <h4 className='text-purple-500 hover:opacity-50 cursor-pointer'>
+              assumenda
+            </h4>
+          </div>
+        </div>
+      </div> */}
+
+      <div className='resources absolute top-2 left-10 px-4 h-60 w-96 flex flex-col justify-evenly'>
+        <div className='flex'>
+          <MdInfoOutline size={20} color={'purple'} />
+          <h3>Reprehenderit</h3>
+        </div>
+
+        <div className='flex'>
+          <MdPeopleAlt size={20} color={'purple'} />
+          <h3>Temporibus</h3>
+        </div>
+      </div>
+
+      {/* <div className='absolute top-4 left-12'>dnkjrewoiq</div> */}
     </div>
   );
 };
